b677dfd53428464d392f3deb5c25fec8c5464a78
Dusan Vejin <dutekvejin@gmail.com>
2017-10-15 04:02:36 +0200

    Improved AbstractHandlerTest.

--- tests/Handler/AbstractHandlerTest.php
+++ tests/Handler/AbstractHandlerTest.php
@@ -4,65 +4,57 @@ namespace WeCodeIn\ErrorHandling\Handler\Tests;
 
 use phpmock\phpunit\PHPMock;
 use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_Matcher_Invocation as Invocation;
 use WeCodeIn\ErrorHandling\Handler\AbstractHandler;
 
 final class AbstractHandlerTest extends TestCase
 {
     use PHPMock;
 
-    /** @var \PHPUnit_Framework_MockObject_MockObject */
-    protected $handler;
-
-    public function setUp()
-    {
-        $this->handler = $this->getMockForAbstractClass(AbstractHandler::class);
-    }
-
     public function testRegisterInvokesRegisterListener()
     {
-        $this->handler->expects($this->once())
-            ->method('registerListener');
-
-        $this->handler->register();
+        $handler = $this->getMockForHandler($this->once());
+        $handler->register();
     }
 
     public function testRegisterWithConsecutiveCallsInvokesRegisterListenerOnce()
     {
-        $this->handler->expects($this->once())
-            ->method('registerListener');
+        $handler = $this->getMockForHandler($this->once());
 
         for ($i = 0; $i < 2; $i++) {
-            $this->handler->register();
+            $handler->register();
         }
     }
 
     public function testRestoreWhenRegisteredInvokesRestoreListener()
     {
-        $this->handler->register();
-
-        $this->handler->expects($this->once())
-            ->method('restoreListener');
-
-        $this->handler->restore();
+        $handler = $this->getMockForHandler($this->once());
+        $handler->register();
+        $handler->restore();
     }
 
     public function testRestoreWhenNotRegisteredNotInvokesRestoreListener()
     {
-        $this->handler->expects($this->never())
-            ->method('restoreListener');
-
-        $this->handler->restore();
+        $handler = $this->getMockForHandler($this->never());
+        $handler->restore();
     }
 
     public function testRestoreWithConsecutiveCallsWhenRegisteredInvokesRestoreListenerOnce()
     {
-        $this->handler->register();
-
-        $this->handler->expects($this->once())
-            ->method('restoreListener');
+        $handler = $this->getMockForHandler($this->once());
+        $handler->register();
 
         for ($i = 0; $i < 2; $i++) {
-            $this->handler->restore();
+            $handler->restore();
         }
     }
+
+    protected function getMockForHandler(Invocation $matcher)
+    {
+        $handler = $this->getMockForAbstractClass(AbstractHandler::class);
+        $handler->expects($matcher)
+            ->method('registerListener');
+        return $handler;
+    }
+
 }
